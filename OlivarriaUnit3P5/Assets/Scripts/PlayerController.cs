488854d96a799e98e2a13c4642d8ccbd40fedfea
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2024-04-16 13:43:26 -0700

    Im almost done

--- OlivarriaUnit3P5/Assets/Scripts/PlayerController.cs
+++ OlivarriaUnit3P5/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
     }
@@ -19,17 +24,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        } 
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("GameOver");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+        }
     }
 }
